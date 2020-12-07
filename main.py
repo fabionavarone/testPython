44c0f3dde472a41f90f227db51d181ba04a1c24e
Fabio Garcia <fabiogarcia@MacBook-Air-de-Fabio.local>
2020-12-07 00:40:48 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+while True:
+	passwordFile = open('SecretPasswordFile.txt')
+	secretPassword = passwordFile.read()
+	print('Enter you password')
+
+	while True:
+
+		typePassword = input()
+		if typePassword == secretPassword:
+			print('Acess granted')
+		if typePassword == '12345':
+			print('That password is one that an idiot puts on their luggage.')
+			break
+		else:
+			print('Acess denied')
+
+		
+print('Saiu do programa')
